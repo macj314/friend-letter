865edc0d0f3184a6c300ae2c788c481a0d3c48e0
unknown <macjay314@gmail.com>
2020-05-18 11:19:41 -0700

    Add location and souvenir variables

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -17,16 +17,20 @@ namespace FriendLetter.Controllers
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
       myLetterVariable.Sender = "Jasmine";
+      myLetterVariable.Location = "Tortuga";
+      myLetterVariable.Souvenir = "CLAMS";
       return View(myLetterVariable);
     }
     [Route("/form")]
     public ActionResult Form() { return View(); }
     [Route("/postcard")]
-    public ActionResult Postcard(string recipient, string sender)
+    public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
     {
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = recipient;
       myLetterVariable.Sender = sender;
+      myLetterVariable.Location = location;
+      myLetterVariable.Souvenir = souvenir;
       return View(myLetterVariable);
     }
   }
